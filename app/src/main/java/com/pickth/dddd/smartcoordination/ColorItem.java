e7fc5ace7be12211cdd9f1f15d97b0e639afa2fa
HaEun <hagomy@naver.com>
2019-03-31 21:12:03 +0900

    Add custom spinner in activity_cloth_add.xml

---
+++ app/src/main/java/com/pickth/dddd/smartcoordination/ColorItem.java
@@ -0,0 +1,19 @@
+package com.pickth.dddd.smartcoordination;
+
+public class ColorItem {
+    private String mColorName;
+    private int mColor;
+
+    public ColorItem(String colorName, int color){
+        mColorName = colorName;
+        mColor = color;
+    }
+
+    public String getColorName(){
+        return mColorName;
+    }
+
+    public int getColor(){
+        return mColor;
+    }
+}
