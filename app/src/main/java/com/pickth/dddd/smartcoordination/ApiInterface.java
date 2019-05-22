a7484a0e464e162b37c2b4352768a3a6cef2a2e3
Jinsil117 <jinsil117@nate.com>
2019-05-22 09:46:22 +0900

    related potoshop file delete

--- app/src/main/java/com/pickth/dddd/smartcoordination/ApiInterface.java
+++ app/src/main/java/com/pickth/dddd/smartcoordination/ApiInterface.java
@@ -8,7 +8,10 @@ import retrofit2.http.Query;
  * Created by parkjinsil on 2019-03-21.
  */
 
+//위도와 경도를 받아오기 위한 api 인터페이스
 public interface ApiInterface {
+    //openweathermap에 데이터를 받아오도록 하는
     @GET("/data/2.5/weather")
+    //위도와 경도를 받아오며, CoordiRepo를 호출한다.
     Call<CoordiRepo> repo(@Query("appid") String appid, @Query("lat") double lat, @Query("lon") double lon);
 }
